b238fc22a0835c761df7dc643f1e07810de56923
Andreas K. Berg <andreaskb98@gmail.com>
2021-09-21 12:18:15 +0200

    - #3: Move constraints, update makefile and gitignore 	* Move constraints file away from Vivado controlled directory 	* Update log file creation in makefile

--- Makefile
+++ Makefile
@@ -1,22 +1,38 @@
 
 PROJECT   = DMPRO_audio
-SYNTH_DIR = logs/synth_$(shell date +"%y%m%d%H%M")
-FLASH_DIR = logs/flash_$(shell date +"%y%m%d%H%M")
+OUTPUT_DIR= $(PWD)/design_output
+SYNTH_DIR = $(PWD)/logs/synth_$(shell date +"%y%m%d%H%M")
+FLASH_DIR = $(PWD)logs/flash_$(shell date +"%y%m%d%H%M")
+TEMP_DIR  = $(OUTPUT_DIR)/tmp
+
+TCL_ARGS = \
+	$(PROJECT) \
+	$(SYNTH_DIR) \
+	$(OUTPUT_DIR)
 
 all:
-	-mkdir -p logs
-	-mkdir -p $(SYNTHDIR)
+	
+	-mkdir -p $(OUTPUT_DIR)
+	-mkdir -p $(SYNTH_DIR)
 	vivado \
 		-mode batch \
 		-source build.tcl \
 		-journal $(SYNTH_DIR)/vivado_build.jou \
-		-log $(SYNTH_DIR)/vivado_build.log
+		-log $(SYNTH_DIR)/vivado_build.log \
+		-tclargs $(TCL_ARGS) \
+		-tempDir $(TEMP_DIR)
+	-rm usage_statistics*
 
 program:
-	-mkdir -p logs
 	-mkdir -p $(FLASH_DIR)
 	vivado \
 		-mode batch \
 		-source program.tcl \
 		-journal $(FLASH_DIR)/vivado_program_$(shell date +"%y%m%d%H%M").jou \
-		-log $(FLASH_DIR)/vivado_program_$(shell date +"%y%m%d%H%M").log
+		-log $(FLASH_DIR)/vivado_program_$(shell date +"%y%m%d%H%M").log \
+		-tclargs $(TCL_ARGS) \
+		-tempDir $(TEMP_DIR)
+	-rm usage_statistics*
+	
+purge:
+	-rm -rf design_output logs usage_statistics*
--- build.tcl
+++ build.tcl
@@ -1,49 +1,62 @@
+#
+# BUILD SCRIPT
+#
+
+# Get design name from arguments
+set design_name [lindex $argv 0]
+# Get output directory from arguments
+set output_dir [lindex $argv 2]
+
 # Assemble project
 
-# read_verilog [glob ./rtl_modules/shift_registers/sipo_register.v]
-# read_verilog [glob ./rtl_modules/spi_slave.v]
+# Sources
+read_verilog [glob ./rtl_modules/shift_registers/sipo_register.v]
+read_verilog [glob ./rtl_modules/spi_slave.v]
 read_verilog [glob ./rtl_modules/top.v]
 
 # Board constaints file
-read_xdc ./AudioSampler.srcs/constrs_1/imports/digilent-xdc-master/Arty-A7-35-Master.xdc
+read_xdc ./constraints/Arty-A7-35-Master.xdc
 
 # Other constraints
 set_property SEVERITY {Warning} [get_drc_checks NSTD-1]
 set_property SEVERITY {Warning} [get_drc_checks UCIO-1]
 
+
 # Synthesis and implementation
+
 set ARTYA735T "xc7a35ticsg324-1L"
 
-set outputDir ./design_output
-file mkdir -p $outputDir
-
 # Synthesis
-synth_design -name my_design -verilog_define DEBUG=1 -top top -part $ARTYA735T
-# Write checkpoint and report
-write_checkpoint -force $outputDir/post_synth
-report_utilization -file $outputDir/post_synth_util.rpt
-report_timing -sort_by group -max_paths 5 -path_type summary -file $outputDir/post_synth_timing.rpt
 
+synth_design -name $design_name -verilog_define DEBUG=1 -top top -part $ARTYA735T
+# Write checkpoint and report
+write_checkpoint -force $output_dir/post_synth
+report_utilization -file $output_dir/post_synth_util.rpt
+report_timing -sort_by group -max_paths 5 -path_type summary -file $output_dir/post_synth_timing.rpt
 # Optimize
 opt_design
 power_opt_design
 # Placement
 place_design
 # Write checkpoint
-write_checkpoint -force $outputDir/post_place
+write_checkpoint -force $output_dir/post_place
 phys_opt_design
 # Route
 route_design
 # Write checkpoint
-write_checkpoint -force $outputDir/post_place
+write_checkpoint -force $output_dir/post_place
+
 
 # Generate reports
-report_timing_summary -file $outputDir/post_route_timing_summary.rpt
-report_timing -sort_by group -max_paths 100 -path_type summary -file $outputDir/post_route_timing.rpt
-report_utilization -file $outputDir/post_route_util.rpt
-report_drc -file $outputDir/post_impl_drc.rpt
-write_verilog -force $outputDir/post_impl_netlist.v
-write_xdc -no_fixed_only -force $outputDir/post_impl.xdc
+
+report_timing_summary -file $output_dir/post_route_timing_summary.rpt
+report_timing -sort_by group -max_paths 100 -path_type summary -file $output_dir/post_route_timing.rpt
+report_utilization -file $output_dir/post_route_util.rpt
+report_drc -file $output_dir/post_impl_drc.rpt
+write_verilog -force $output_dir/post_impl_netlist.v
+write_xdc -no_fixed_only -force $output_dir/post_impl.xdc
+
 
 # Generate bitstream
-write_bitstream -force -file $outputDir/program.bit
+
+write_bitstream -force -file $output_dir/program.bit
