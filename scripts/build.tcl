b4e2b6c3a3c8d9f6c69e313c5fb4cd5f2599fc0e
LarsAur <lars1998@hotmail.com>
2021-10-05 16:25:03 +0200

    Add MMCM module from clocking wizard

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -19,8 +19,9 @@ set synth_only [lindex $argv 5]
 read_verilog -sv [glob $source_dir/shift_registers/sipo_register.sv]
 read_verilog -sv [glob $source_dir/spi_slave.sv]
 read_verilog -sv [glob $source_dir/fifo_delay.sv]
-read_verilog -sv [glob $source_dir/oscillator.sv]
+#read_verilog -sv [glob $source_dir/oscillator.sv]
 read_verilog -sv [glob $source_dir/top.sv]
+read_verilog [glob $source_dir/clk_wiz/clk_wiz.v]
 
 add_files [glob $source_dir/../lookup_tables/*]
 # ! 
