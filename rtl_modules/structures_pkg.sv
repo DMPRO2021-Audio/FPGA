8c780461599588028425eb1af5b32bbe7d0c9d55
Andreas K. Berg <andreaskb98@gmail.com>
2021-10-07 10:49:42 +0200

    Add shape_pkg and reset_synth_t function

--- rtl_modules/structures_pkg.sv
+++ rtl_modules/structures_pkg.sv
@@ -1,5 +1,13 @@
+`include "constants.svh"
 
-
+package shape_pkg;
+    typedef enum logic [1:0]{
+        SAWTOOTH = 0,
+        SQUARE,         
+        SIN,           
+        SAMPLE_NAME     // Not implemented, this can be any sample
+    } wave_shape;
+endpackage
 package protocol_pkg;
     //---------------------------------------------------------------------------------------------/
     // Definition of protocol for communication between MCU and FPGA
@@ -8,19 +16,32 @@ package protocol_pkg;
 
     typedef struct packed {
         logic [31:0] rate;
-        logic [31:0] time;
+        logic [31:0] duration;
     } envelope_t;
 
     typedef struct packed {
-        envelope_t [`ENVELOPE_LEN] envelopes;
+        envelope_t [0:`ENVELOPE_LEN-1]envelopes;
         logic [31:0] freq;
         wave_shape shape;
     } wavegen_t;
 
     typedef struct packed {
-        wavegen_t [`N_OSCILLATORS] wave_gens;
+        wavegen_t [0:`N_OSCILLATORS-1] wave_gens;
         logic [31:0] reverb;
         logic [31:0] volume;
     } synth_t;
 
-endpackage;
+    function void reset_synth_t(output synth_t synth);
+        synth.reverb = 0;
+        synth.volume = 0;
+        for (int i = 0; i < `N_OSCILLATORS; i++) begin
+            synth.wave_gens[i].freq = 0;
+            synth.wave_gens[i].shape = SIN;
+            for (int ii = 0; ii < `ENVELOPE_LEN; ii++) begin
+                synth.wave_gens[i].envelopes[ii].rate = 0;
+                synth.wave_gens[i].envelopes[ii].duration = 0;
+            end
+        end
+    endfunction
+
+endpackage
