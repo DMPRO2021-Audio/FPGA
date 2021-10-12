1a72d64eb84686cb41765af629c462ef334345d8
LarsAur <lars1998@hotmail.com>
2021-10-12 12:50:14 +0200

    Include clk_wiz and oscillator

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -19,9 +19,13 @@ set synth_only [lindex $argv 5]
 read_verilog -sv [glob $source_dir/shift_registers/sipo_register.sv]
 read_verilog -sv [glob $source_dir/spi_slave.sv]
 read_verilog -sv [glob $source_dir/fifo_delay.sv]
-#read_verilog -sv [glob $source_dir/oscillator.sv]
+read_verilog -sv [glob $source_dir/oscillator.sv]
+read_verilog [glob $source_dir/I2S_transmitter.v]
 read_verilog -sv [glob $source_dir/top.sv]
-read_verilog [glob $source_dir/clk_wiz/clk_wiz.v]
+# Use this when using 48MHz clock
+#read_verilog [glob $source_dir/clk_wiz/clk_wiz.v]  
+# Use this when using 100MHz clock (devboard)
+read_verilog [glob $source_dir/clk_wiz/clk_wiz_dev.v] 
 
 add_files [glob $source_dir/../lookup_tables/*]
 # ! 
