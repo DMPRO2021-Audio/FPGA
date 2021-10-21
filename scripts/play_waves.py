8bf06bcfff89391f723a28dd1378e5f35ac1f75f
Andreas K. Berg <andreaskb98@gmail.com>
2021-10-21 15:06:07 +0200

    Take cmd argument

--- scripts/play_waves.py
+++ scripts/play_waves.py
@@ -1,10 +1,14 @@
 import numpy as np
 import simpleaudio as sa
+import sys
 
 sample_rate = 48000 # Hz
 
+path = sys.argv[1] if len(sys.argv) > 1 else "../testbenches/test_output/oscillator.txt"
+
+
 # Import generated waves
-y = np.genfromtxt("../testbenches/test_output/oscillator.txt", delimiter=",", unpack=True)
+y = np.genfromtxt(path, delimiter=",", unpack=True)
 x = np.linspace(0, len(y), len(y))
 # normalize to 16-bit range
 y *= 32767 / np.max(np.abs(y))
